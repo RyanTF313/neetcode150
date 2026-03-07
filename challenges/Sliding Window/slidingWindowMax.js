a0a04bef27ec47b5b5c66c785fbd895b601899a8
RyanTF313 <r.tillman.french@gmail.com>
2026-03-06 20:47:35 -0500

    remove comments

--- challenges/Sliding Window/slidingWindowMax.js
+++ challenges/Sliding Window/slidingWindowMax.js
@@ -5,19 +5,16 @@ const maxSlidingWindow = (nums, k) => {
   const store = [];
 
   for (let i = 0; i < nums.length; i++) {
-    // Remove indices out of current window
     while (store.length && store[0] <= i - k) {
       store.shift();
     }
 
-    // Maintain decreasing order in store
     while (store.length && nums[store[store.length - 1]] <= nums[i]) {
       store.pop();
     }
 
     store.push(i);
 
-    // Record max once first window is formed
     if (i >= k - 1) {
       result.push(nums[store[0]]);
     }
